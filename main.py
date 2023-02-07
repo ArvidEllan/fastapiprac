0049ce945ba49db87a485e470895268175083589
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-07 09:07:55 +0300

    update on item fetching

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 app=FastAPI()
 
-class Item(BaseModel):
+class Item(BaseModel): #serializer
     id:int
     name:str
     description:str
@@ -19,3 +19,9 @@ def index():
 def greet_name(name:str):
    return {"greeting":f"hello {name}"}
 
+@app.put('/item/{item_id}')
+def update_item(item_id:int,item:Item):
+    return {'name':item.name,
+            'description':item.description,
+            'price':item.price,
+            'on_offer': item.on_offer}
