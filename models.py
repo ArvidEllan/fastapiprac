925ad8e64ec935e4d6a7ce887061646cc4eeef60
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-12 19:26:53 +0300

    class item on the database

--- models.py
+++ models.py
@@ -4,3 +4,7 @@ from sqlalchemy import String,Boolean,Integer,Column
 
 Class Item(Base):
     __tablename__='items'
+    id=Column(Integer,primary_key=True)
+    name=Column(String(255),nullable=False,unique=True)
+    description=Column(Text)
+    on_offer=Column(Boolean,default=False)
