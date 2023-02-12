e7dc5967ec6e5aeac7ca382d937cfda22c066653
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-12 19:20:38 +0300

    base declarative class

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 #pip install psycopg2-binary ---python connector postgresql to mysql
 
 engine = create_engine("postgresql://postgres:password@localhost/dbname")
+    echo=True 
+    
+Base = declarative_base()
